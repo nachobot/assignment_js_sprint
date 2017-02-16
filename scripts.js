7708dc72258331a67d9e686bbfd42e44b1dfec02
Daniella D'Souza <danielladsou@gmail.com>
2017-02-16 12:57:38 -0600

    Implementation of compareArrays
    Works!

--- scripts.js
+++ scripts.js
@@ -51,9 +51,25 @@ var sprintFunctions = {
   
   },
 
-  compareArrays: function(){ 
+  compareArrays: function(array1, array2){ 
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    if (array1.length != array2.length){
+
+     return false;
+    
+    }
+    
+    for (let i = 0; i < array1.length; i++){
+
+     if (array1[i] != array2[i]) {
+
+          return false;
+        
+      }
+   
+    } 
+    return true;
+  
   },
 
   fizzBuzz: function(){  
